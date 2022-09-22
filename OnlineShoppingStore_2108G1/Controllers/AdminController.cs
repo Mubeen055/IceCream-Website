c9bd7ddcba178cdda989f73c8872d578db77b217
MuhammadMubeen <faizmubeen105@gmail.com>
2022-09-22 20:16:03 +0500

    v-1.0

---
+++ OnlineShoppingStore_2108G1/Controllers/AdminController.cs
@@ -0,0 +1,87 @@
+﻿using OnlineShoppingStore_2108G1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore_2108G1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ecommerce_AUHEntities db = new ecommerce_AUHEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["LoggedAdminName"] == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            ViewBag.TotalUsers = db.Users.Count();
+            ViewBag.TotalOrders = db.Orders.Count();
+            ViewBag.TotalEarnnings = db.Orders.Where(a => a.Order_Status.Equals("Completed"))
+                                               .Select(a => a.Total_Amount).Sum();
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            ViewBag.CategoriesList = new SelectList(db.Categories, "Category_ID", "Category_Name");
+            if (Session["LoggedAdminName"] == null)
+            {
+                return RedirectToAction("Admin", "Home");
+                }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var obj = db.Users.Where(x => x.Email.Equals(user.Email)
+                            && x.Password.Equals(user.Password)
+                            && x.Email_Verified.Equals("Y")
+                            && x.Role.Equals("Admin")).FirstOrDefault();
+                if (obj != null) //On success
+                {
+
+                    Session["LoggedAdminName"] = obj.Name;
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            ModelState.AddModelError("InvalidLogin", "Email or Password is incorrect!");
+            return View();
+        }
+
+            [HttpPost]
+            public ActionResult AddProduct(Product product,HttpPostedFileBase file)
+            {
+                ViewBag.CategoriesList = new SelectList(db.Categories, "Category_ID", "Category_Name", product.Category_ID);
+                if(ModelState.IsValid)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                    string extension = Path.GetExtension(file.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmss") + extension;
+                    product.Picture = "/External/Main/Product/" + fileName;
+                    fileName = Path.Combine(Server.MapPath("/External/Main/Product/"), fileName);
+
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+                return View();
+            }
+
+
+
+        }
+    }
---
+++ OnlineShoppingStore_2108G1/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using OnlineShoppingStore_2108G1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;    
+
+namespace OnlineShoppingStore_2108G1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ecommerce_AUHEntities db = new ecommerce_AUHEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Register(User obj)
+        {
+            if(ModelState.IsValid)
+            {
+                //Email Exists Validation
+                var isEmailExists = db.Users.Any(x => x.Email == obj.Email);                              
+                if(isEmailExists == true)
+                {
+                    ModelState.AddModelError("EmailExists","This email already exists!");
+                    return View(obj);
+                }
+
+                //Password, Confirm Password Validation
+                if(obj.Password != obj.ConfirmPassword)
+                {
+                    ModelState.AddModelError("PasswordNotSame", "Both passwords must match!");
+                    return View(obj);
+                }
+
+                obj.Email_Verified = "N";
+                obj.Registration_Date = DateTime.Now;
+                obj.Role = "Customer";
+
+                db.Users.Add(obj);
+                db.SaveChanges();
+
+                TempData["Success"] = "<script> alert('Your account has been created!'); </script>";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var obj = db.Users.Where(x=>x.Email.Equals(user.Email)
+                            && x.Password.Equals(user.Password)
+                            && x.Email_Verified.Equals("Y")).FirstOrDefault();
+               if(obj != null) //On success
+               {
+                    Session["LoggedInUserID"] = obj.User_ID;
+                    Session["LoggedInUserEmail"] = obj.Email;
+                    Session["LoggedInUserAddress"] = obj.Address;
+                    Session["LoggedInUserContact"] = obj.Contact_Number;
+                    Session["LoggedInUserName"] = obj.Name;
+                    Session["LoggedInUserCity"] = obj.City;
+
+
+                    TempData["Success"] = "<script> alert('Successfully logged in!'); </script>";
+                    return RedirectToAction("Index","Home");
+               }                
+            }
+            ModelState.AddModelError("InvalidLogin", "Email or Password is incorrect!");
+            return View();
+        }
+
+        public ActionResult Shop(int? page)
+        {
+            ViewBag.CategoriesList = new SelectList(db.Categories, "Category_ID", "Category_Name");
+
+            Products_WithFilter m = new Products_WithFilter();
+            int pagSize = 5;
+            int pageNumber = (page ?? 1);
+            m.ProductList = db.Products
+                           .OrderBy(a => a.Product_id)
+                           .ToPagedList(pageNumber, pagSize);
+
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult Shop(int? page, decimal min_price, decimal max_price, string CategoriesList)
+        {
+            ViewBag.CategoriesList = new SelectList(db.Categories, "Category_ID", "Category_Name", CategoriesList);
+
+            Products_WithFilter m = new Products_WithFilter();
+            int pagSize = 5;
+            int pageNumber = (page ?? 1);
+
+            if(CategoriesList == "")
+            {
+                m.ProductList = db.Products.OrderBy(a => a.Product_id).Where(a => a.Price >= min_price && a.Price <= max_price).ToPagedList(pageNumber, pagSize);
+            }
+            else
+            {
+                int ctID = Convert.ToInt32(CategoriesList);
+                m.ProductList = db.Products.OrderBy(a => a.Product_id).Where(a => a.Price >= min_price && a.Price <= max_price && a.Category_ID == ctID).ToPagedList(pageNumber, pagSize);
+            }
+        
+
+            return View(m);
+        }
+
+
+    }
+}
